5d39097226e99d11ba977bbbb7002860fbbdaeb5
Knut Franke <Knut.Franke@gmx.de>
2010-09-12 20:37:05 +0200

    add simple Qt-based model/view demo
    Compared to defining a change notification Qt signal in the model and connecting
    that to the Label, we can see that with qruby-cells the code becomes
    less cluttered. We simply state the logical relationship between model
    and view, i.e. that output.text should always be a certain function of
    the model state, instead of having to figure out how to keep the output
    in sync with the model.
    
    While in this simple case only a couple of lines have been saved,
    imagine what happens if your view has multiple output elements depending
    on multiple model properties each.

---
+++ model-view.rb
@@ -0,0 +1,46 @@
+$:.unshift '.'
+require 'cells'
+require 'Qt4'
+
+# A simple model class.
+# Imagine some interesting domain logic here.
+class Model
+	cell :content
+	def initialize
+		self.content = "Hello World."
+	end
+end
+
+# A simple view to match our simple model.
+class View < Qt::Widget
+	def initialize(model)
+		super()
+
+		self.window_title = 'Testing ruby-cells with a model/view application'
+		resize(500,100)
+		addAction(Qt::Action.new(self) do
+			self.shortcuts = Qt::KeySequence.Quit
+			connect(SIGNAL "triggered(bool)") { Qt::Application.instance.quit }
+		end)
+
+		input = Qt::LineEdit.new(model.content) do
+			connect(SIGNAL "textChanged(QString)") { model.content = text }
+		end
+		output = Qt::Label.new do
+			# This is the interesting bit:
+			calculate(:text) { "+++" + model.content + "+++" }
+		end
+
+		self.layout = Qt::VBoxLayout.new do
+			add_widget input
+			add_widget output
+		end
+	end
+end
+
+Qt::Application.new(ARGV) do
+	demo = View.new(Model.new)
+	demo.show
+	exec
+end
+
